bb162462d182a3ae34b65acf8b1689f3bdb17b0b
Tsu <tundgch200074@fpt.edu.vn>
2022-08-16 22:57:50 +0700

    Seed User Role
    Startup.cs
    ApplicationDbContext.cs
    set up

--- Demo/Data/ApplicationDbContext.cs
+++ Demo/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Demo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -25,9 +26,73 @@ namespace Demo.Data
             SeedCategory(builder);
             SeedAuthor(builder);
 
+            SeedUser(builder);
+            SeedRole(builder);
+            SeedUserRole(builder);
 
         }
+        private void SeedUserRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    UserId = "1",
+                    RoleId = "1"
+                },
+                new IdentityUserRole<string>
+                {
+                    UserId = "2",
+                    RoleId = "2"
+                }
+            );
+        }
 
+        private void SeedRole(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "Admin"
+                },
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "Customer",
+                    NormalizedName = "Customer"
+                }
+            );
+        }
+
+        private void SeedUser(ModelBuilder builder)
+        {
+            //tạo tài khoản test cho admin & customer
+            var admin = new IdentityUser
+            {
+                Id = "1",
+                Email = "admin@gmail.com",
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "admin@gmail.com"
+            };
+            var customer = new IdentityUser
+            {
+                Id = "2",
+                Email = "customer@gmail.com",
+                UserName = "customer@gmail.com",
+                NormalizedUserName = "customer@gmail.com"
+            };
+
+            //khai báo thư viện để mã hóa mật khẩu cho user
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            //set mật khẩu đã mã hóa cho từng user
+            admin.PasswordHash = hasher.HashPassword(admin, "123456");
+            customer.PasswordHash = hasher.HashPassword(customer, "123456");
+
+            //add 2 tài khoản test vào bảng User
+            builder.Entity<IdentityUser>().HasData(admin, customer);
+        }
         private void SeedBook(ModelBuilder builder)
         {
             builder.Entity<Book>().HasData(
